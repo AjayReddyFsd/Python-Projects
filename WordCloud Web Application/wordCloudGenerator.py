993ef2915ee8d02f760559c7f682d679f99876da
AjayReddyFsd <ajayreddy.fsd@gmail.com>
2022-10-26 22:29:45 -0400

    WordCloud Web Application Done

--- WordCloud Web Application/wordCloudGenerator.py
+++ WordCloud Web Application/wordCloudGenerator.py
@@ -16,7 +16,7 @@ def wordCloudGenerator():
     if text != '':
         wordCloud = WordCloud().generate(text)
         plt.imshow(wordCloud)
-        plt.axis('off')
+        plt.axis('off')  # to remove the axes on the plot
         st.pyplot()
 
 
