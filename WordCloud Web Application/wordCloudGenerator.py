f3b179d60663baba18071f4d1ee3e7c144e2b378
AjayReddyFsd <ajayreddy.fsd@gmail.com>
2022-10-26 22:28:44 -0400

    WordCloud Web Application Done

---
+++ WordCloud Web Application/wordCloudGenerator.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+
+def wordCloudGenerator():
+    # to create heading in the web app
+    st.subheader('WordCloud WebApp')
+
+    # to take userInput from the web app
+    text = st.text_input('Please enter the text and press enter')
+
+    # some terminal recommended code
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+
+    if text != '':
+        wordCloud = WordCloud().generate(text)
+        plt.imshow(wordCloud)
+        plt.axis('off')
+        st.pyplot()
+
+
+wordCloudGenerator()
